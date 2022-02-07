04c493965c7aaa1bcb3c93d81a379f9cb5b0dd37
Oleg Khmelevskoy <olegileto@gmail.com>
2022-02-07 15:44:34 +0200

    Minor fix.

--- src/helpers/serviceWorkers.js
+++ src/helpers/serviceWorkers.js
@@ -2,7 +2,13 @@ import { STATUS_OK } from './statuses';
 
 const Api = {
     get: async(url = '') => {
-        const data = await fetch(url).then(
+        const data = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+              },
+            redirect: 'follow'
+        }).then(
             response => {
                 if (response.status === STATUS_OK) {
                     return response.json().then(data => data);
