311ce6295363a2cb2294ede0b0ea103b15a9f800
Oleg Khmelevskoy <olegileto@gmail.com>
2022-02-08 09:48:54 +0200

    Fixed photo scrolling.

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,6 +1,3 @@
 export const getPhotoById = (id, photos) => {
     return photos?.find(photo => photo.id === id);
 };
-
-export const chunkDataIntoColumns = (data, chunkSize) => 
-[...Array(chunkSize).keys()].map(chunk => data?.filter((_, i) => i % chunkSize === chunk));
